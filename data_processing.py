98a53fc51338834af329b00362a90bd0e4ff1aad
satinmalik <malik.satin@gmail.com>
2022-04-07 16:26:04 +0530

    correct file added

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,22 @@
 
 print("adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a + b
+    return c
+print("sum function")
+
+def multiplication(a,b):
+    d = a*b
+    return d
+
+print("multiplication function")
+
+def division(a,b):
+    e = a/b
+    return e
+
+print("division function")
